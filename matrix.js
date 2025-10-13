7e52ae71f1dc91e3fe9424efac2e6fee5595b961
leonardo-rivero <leonardo.rivero.l@ucb.edu.bo>
2025-10-13 08:19:08 -0400

    Crear matrix.js

---
+++ matrix.js
@@ -0,0 +1,21 @@
+class Matrix {
+  rows;
+  cols;
+  data;
+
+  constructor(rowsParam, colsParam, defaultValue) {
+    this.rows = rowsParam;
+    this.cols = colsParam;
+    this.data = [];
+
+    for (let i = 0; i < rowsParam; i++) {
+      const rowTemp = [];
+      for (let j = 0; j < colsParam; j++) {
+        rowTemp.push(defaultValue);
+      }
+      this.data.push(rowTemp);
+    }
+  }
+
+  // Función de validación de rango válido en la matriz
+  isValidPosit
